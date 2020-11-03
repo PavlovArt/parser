472c7b3487e4a03fea2bf2bce07d6bcaefffee54
Artem Pavlov <pavlovart11@gmail.com>
2020-11-03 17:59:15 +0200

    added parser

--- parser/parser/ViewController.swift
+++ parser/parser/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var users = [UserData]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        downloadJSON {
+            print("successful")
+        }
         // Do any additional setup after loading the view.
     }
 
+    func downloadJSON(completed: @escaping () -> ()) {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error  == nil {
+                do {
+                    self.users = try JSONDecoder().decode([UserData].self, from: data!)
+                    //reload tableview
+                    DispatchQueue.main.async {
+                        completed()
+                    }
+                } catch {
+                    print("JSON error")
+                }
+            }
+        }.resume()
+    }
 
 }
 
