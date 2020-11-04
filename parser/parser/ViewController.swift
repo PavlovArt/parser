eada353d6622ce91b808cf2024d63ccf1fb2ab2a
Artem Pavlov <pavlovart11@gmail.com>
2020-11-04 18:26:50 +0200

    Added tableview and passing data to table

--- parser/parser/ViewController.swift
+++ parser/parser/ViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var users = [UserData]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         downloadJSON {
+            self.tableView.reloadData()
             print("successful")
         }
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = users[indexPath.row].name.capitalized
+        return cell
     }
 
     func downloadJSON(completed: @escaping () -> ()) {
