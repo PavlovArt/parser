680f21d663524eea07f0d102214ddd79bed41f15
Pavlov Artem <pavlovart11@gmail.com>
2020-11-14 15:27:01 +0200

    Deleted all relations to parsing from 3rd party service

--- parser/parser/ViewController.swift
+++ parser/parser/ViewController.swift
@@ -11,16 +11,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     @IBOutlet weak var tableView: UITableView!
-    var users = [UserData]()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        downloadJSON {
-            self.tableView.reloadData()
-            print("successful")
-        }
-        // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -34,23 +27,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = users[indexPath.row].email
         return cell
     }
-
-    func downloadJSON(completed: @escaping () -> ()) {
-        let url = URL(string: "https://jsonplaceholder.typicode.com/users")
-        URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            if error  == nil {
-                do {
-                    self.users = try JSONDecoder().decode([UserData].self, from: data!)
-                    //reload tableview
-                    DispatchQueue.main.async {
-                        completed()
-                    }
-                } catch {
-                    print("JSON error")
-                }
-            }
-        }.resume()
-    }
-
 }
 
